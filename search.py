a1ad6523082da5e4e8844317f9bd7131cc6f9e44
Sam <splavigne@gmail.com>
2014-05-26 20:31:26 -0400

    getting better

---
+++ search.py
@@ -0,0 +1,87 @@
+import random
+from pattern.search import Pattern, STRICT, search
+from pattern.en import parsetree, wordnet
+
+def re_search(text, search_string, strict=False):
+    tree = parsetree(text, lemmata=True)
+    if strict:
+        results = search(search_string, tree, STRICT)
+    else:
+        results = search(search_string, tree)
+    return results
+
+
+def search_out(text, search_string, strict=False):
+    results = re_search(text, search_string, strict)
+    output = []
+    for match in results:
+        sent = []
+        for word in match:
+            sent.append(word.string)
+        output.append(" ".join(sent))
+    return output
+
+def contains(text, search_string):
+    results = re_search(text, search_string)
+    return len(results) > 0
+
+def hypernym_search(text, search_word):
+    output = []
+    synset = wordnet.synsets(search_word)[0]
+    pos = synset.pos
+    possible_words = re_search(text, pos)
+    for match in possible_words:
+        word = match[0].string
+        synsets = wordnet.synsets(word)
+        if len(synsets) > 0:
+            hypernyms = synsets[0].hypernyms(recursive=True)
+            if any(search_word == h.senses[0] for h in hypernyms):
+                output.append(word)
+    return set(output)
+
+
+def hypernym_combo(text, category, search_pattern):
+    possibilities = search_out(text, search_pattern)
+    output = []
+    for p in possibilities:
+        if len(hypernym_search(p, category)) > 0:
+            output.append(p)
+    return output
+
+
+def list_hypernyms(search_word):
+    output = []
+    for synset in wordnet.synsets(search_word):
+        hypernyms = synset.hypernyms(recursive=True)
+        output.append([h.senses[0] for h in hypernyms])
+    return output
+
+
+def random_hyponym(word):
+    to_return = ''
+    hyponyms = list_hyponyms(word)
+    if len(hyponyms) > 0:
+        to_return = random.choice(hyponyms)
+    return to_return
+
+
+def list_hyponyms(word):
+    output = []
+    synsets = wordnet.synsets(word)
+    if len(synsets) > 0:
+        hyponyms = synsets[0].hyponyms(recursive=True)
+        output = [h.senses[0] for h in hyponyms]
+    return output
+
+
+if __name__ == '__main__':
+
+    import sys
+
+    #results = list_hypernyms(sys.argv[1])
+    text = sys.stdin.read()
+    #results = search_out(text, sys.argv[1], True)
+    #results = hypernym_search(text, sys.argv[1])
+    results = hypernym_combo(text, sys.argv[1], sys.argv[2])
+    for result in set(results):
+        print result
--- videogrep.py
+++ videogrep.py
@@ -1,16 +1,21 @@
 FFMPEG_BINARY = '/usr/local/bin/ffmpeg'
 
-import os, sys, re, random
+import os, re, random
+import search as Search
+from collections import OrderedDict
 from moviepy.editor import *
 
+usable_extensions = ['mp4', 'avi', 'mov']
 
 def convert_timespan(timespan):
+    """Converts an srt timespan into a start and end timestamp"""
     start, end = timespan.split('-->')
     start = convert_timestamp(start)
     end = convert_timestamp(end)
     return start, end
 
 def convert_timestamp(timestamp):
+    """Converts an srt timestamp into seconds"""
     timestamp = timestamp.strip()
     chunk, millis = timestamp.split(',')
     hours, minutes, seconds = chunk.split(':')
@@ -20,58 +25,124 @@ def convert_timestamp(timestamp):
     seconds = seconds + hours * 60 * 60 + minutes * 60 + float(millis)/1000
     return seconds
 
-search = sys.argv[1]
-maxclips = int(sys.argv[2])
+def clean_srt(srt):
+    """Removes damaging line breaks and numbers from srt files and returns a dictionary"""
+    f = open(srt, 'r')
+    text = f.read()
+    f.close()
+    text = re.sub(r'^\d+[\n\r]', '', text, flags=re.MULTILINE)
+    lines = text.splitlines()
+    output = OrderedDict()
+    key = ''
 
-srts = [f for f in os.listdir('./') if f.lower().endswith('srt')]
-
-composition = []
-
-for srt in srts:
-    file = open(srt, 'r')
-    lines = file.readlines()
-
-    for i, line in enumerate(lines):
+    for line in lines:
         line = line.strip()
-        if re.search(search, line, re.IGNORECASE):
-            j = 1
-            previous_line = lines[i-j]
-            while (previous_line.find('-->') == -1):
-                j += 1
-                previous_line = lines[i-j]
-            previous_line = previous_line.strip()
-            start, end = convert_timespan(previous_line)
-            composition.append({'file': srt.replace('.srt', '.avi'), 'time': previous_line, 'start': start, 'end': end, 'line': line})
+        if line.find('-->') > -1:
+            key = line
+            output[key] = ''
+        else:
+            if key != '':
+                output[key] += line + ' '
 
-    file.close()
+    return output
 
-clips = []
-composition = composition[:maxclips]
+def demo_supercut(composition):
+    for i, c in enumerate(composition):
+        line = c['line']
+        start = c['start']
+        end = c['end']
+        print "{1} to {2}:\t{0}".format(line, start, end)
 
-print "creating clips"
-time = 0
-for i, c in enumerate(composition):
-    line = c['line']
-    index = line.find(search)
-    position = index / len(line)
-    start = c['start']
-    end = c['end']
-    duration = end - start
+def create_supercut(composition, outputfile):
+    print "Creating clips"
+    clips = []
+    time = 0
+    for i, c in enumerate(composition):
+        line = c['line']
+        start = c['start']
+        end = c['end']
+        duration = end - start
 
-    print "{0}: {1} to {2}".format(line, start, end)
+        print "{1} to {2}:\t{0}".format(line, start, end)
 
-    clip = VideoFileClip(c['file'])
-    clip = clip.subclip(start, end)
-    clip = clip.set_start(time)
-    clip = clip.set_end(time + (end - start))
-    clips.append(clip)
-    time += end - start
+        clip = VideoFileClip(c['file'])
+        clip = clip.subclip(start, end)
+        clip = clip.set_start(time)
+        clip = clip.set_end(time + (end - start))
+        clips.append(clip)
+        time += end - start
 
-print "stitching it together"
-video = concatenate(clips)
+    video = concatenate(clips)
 
-try:
-    print "saving file"
-    video.to_videofile("test_output.mp4")
-except:
-    print 'NO'
+    try:
+        video.to_videofile(outputfile)
+    except:
+        print "Sorry, couldn't output your video file"
+
+
+def search_line(line, search, searchtype):
+    if searchtype == 're':
+        return re.search(search, line, re.IGNORECASE)
+    elif searchtype == 'pos':
+        return Search.search_out(line, search)
+    elif searchtype == 'hyper':
+        return Search.hypernym_search(line, search)
+
+
+def videogrep(inputfile, outputfile, search, searchtype, maxclips, test=False, randomize=False):
+    srts = []
+
+    if os.path.isfile(inputfile):
+        filename = inputfile.split('.')
+        filename[-1] = 'srt'
+        srts = ['.'.join(filename)]
+
+    elif os.path.isdir(inputfile):
+        if inputfile.endswith('/') == False:
+            inputfile += '/'
+        srts = [inputfile + f for f in os.listdir(inputfile) if f.lower().endswith('srt')]
+
+    else:
+        print 'no file found'
+        return False
+
+    composition = []
+
+    for srt in srts:
+        lines = clean_srt(srt)
+        for timespan in lines.keys():
+            line = lines[timespan].strip()
+            if search_line(line, search, searchtype):
+                start, end = convert_timespan(timespan)
+                for ext in usable_extensions:
+                    videofile = srt.replace('.srt', '.' + ext)
+                    if os.path.isfile(videofile):
+                        composition.append({'file': videofile, 'time': timespan, 'start': start, 'end': end, 'line': line})
+
+    composition = composition[:maxclips]
+
+    if randomize == True:
+        random.shuffle(composition)
+
+    if test == True:
+        demo_supercut(composition)
+    else:
+        create_supercut(composition, outputfile)
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Generate a "supercut" of one or more video files by searching through subtitle tracks.')
+    parser.add_argument('--input', '-i', dest='inputfile', required=True, help='video or subtitle file, or folder')
+    parser.add_argument('--search', '-s', dest='search', required=True, help='search term')
+    parser.add_argument('--search-type', '-st', dest='searchtype', default='re', choices=['re', 'pos', 'hyper'], help='type of search')
+    parser.add_argument('--max-clips', '-m', dest='maxclips', default=100, help='maximum number of clips to use for the supercut')
+    parser.add_argument('--output', '-o', dest='outputfile', default='supercut.mp4', help='name of output file')
+    parser.add_argument('--test', '-t', action='store_true', help='show results without making the supercut')
+    parser.add_argument('--randomize', '-r', action='store_true', help='randomize the clips')
+    parser.add_argument('--youtube', '-yt', help='grab clips from youtube based on your search')
+
+    args = parser.parse_args()
+
+    videogrep(inputfile=args.inputfile, outputfile=args.outputfile, search=args.search, searchtype=args.searchtype, maxclips=args.maxclips, test=args.test, randomize=args.randomize)
