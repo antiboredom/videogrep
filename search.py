51e3eded863720458d1e46f35321afec6a1be1d9
Sam <splavigne@gmail.com>
2014-06-09 13:42:20 -0400

    hypernym search can take multiple options

--- search.py
+++ search.py
@@ -27,16 +27,17 @@ def contains(text, search_string):
 
 def hypernym_search(text, search_word):
     output = []
-    synset = wordnet.synsets(search_word)[0]
-    pos = synset.pos
-    possible_words = re_search(text, pos)
-    for match in possible_words:
-        word = match[0].string
-        synsets = wordnet.synsets(word)
-        if len(synsets) > 0:
-            hypernyms = synsets[0].hypernyms(recursive=True)
-            if any(search_word == h.senses[0] for h in hypernyms):
-                output.append(word)
+    for search_word in search_word.split('|'):
+        synset = wordnet.synsets(search_word)[0]
+        pos = synset.pos
+        possible_words = re_search(text, pos)
+        for match in possible_words:
+            word = match[0].string
+            synsets = wordnet.synsets(word)
+            if len(synsets) > 0:
+                hypernyms = synsets[0].hypernyms(recursive=True)
+                if any(search_word == h.senses[0] for h in hypernyms):
+                    output.append(word)
     return set(output)
 
 
