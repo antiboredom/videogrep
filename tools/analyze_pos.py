0b7f4f26404c1bce6934a101c43f73ab982869fc
Sam <splavigne@gmail.com>
2014-06-11 03:14:41 -0400

    make analyze pos work on multiple srt files

--- tools/analyze_pos.py
+++ tools/analyze_pos.py
@@ -1,11 +1,32 @@
 from pattern.en import tag, tokenize
-import sys
+import sys,os,re
 
-text = sys.stdin.read()
+#text = sys.stdin.read()
+inputfile = sys.argv[1]
+srts = []
+text = ''
+if os.path.isfile(inputfile):
+    filename = inputfile.split('.')
+    filename[-1] = 'srt'
+    srts = ['.'.join(filename)]
+
+elif os.path.isdir(inputfile):
+    if inputfile.endswith('/') == False:
+        inputfile += '/'
+    srts = [inputfile + f for f in os.listdir(inputfile) if f.lower().endswith('srt')]
+
+for srt in srts:
+    f = open(srt, 'r')
+    for line in f:
+        if line.find('-->') == -1:
+            text += line
+    f.close()
+
+text = re.sub(r'^\d+[\n\r]', '', text, flags=re.MULTILINE)
 tags = tag(text)
 pos = [t[1] for t in tags]
 ngrams = {}
-n = int(sys.argv[1])
+n = int(sys.argv[2])
 
 for i in range(len(pos) - n + 1):
     gram = tuple(pos[i:i+n])
