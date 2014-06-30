05461f03641af8fec3ad5cf7ec52ff575602fbbd
Dean Silfen <dean.silfen@gmail.com>
2014-06-30 18:06:28 -0400

    more cleanup

--- setup.py
+++ setup.py
@@ -12,10 +12,18 @@ except IOError, ImportError:
 with open('requirements.txt', 'r') as requirements:
     install_requires = requirements.read().split()
 
+'''
+
+Do not use 'djds23' in the final version.
+Before pulling this needs to be changed.
+
+'''
+
 setup(name='videogrep',
       version = '0.4',
       description = 'Python utility for creating video out of their subtitle files',
       long_description = long_description,
+      download_url = 'https://github.com/djds23/videogrep/archive/master.zip'
       author = 'Sam Lavigne',
       author_email = 'splavigne@gmail.com',
       install_requires = install_requires)
