93c65b7b386750fa7118523de4ba87f8cc2602bb
Dean Silfen <dean.silfen@gmail.com>
2014-07-09 17:58:17 -0400

    fixing setup.py

--- setup.py
+++ setup.py
@@ -9,18 +9,11 @@ try:
 except IOError, ImportError:
     long_description =''
 
-'''
-
-Do not use 'djds23' in the final version.
-Before pulling this needs to be changed.
-
-'''
-
 setup(name='videogrep',
       version = '0.4',
       description = 'Python utility for creating video out of their subtitle files',
       long_description = long_description,
-      download_url = 'https://github.com/djds23/videogrep/archive/master.zip',
+      download_url = 'https://github.com/antiboredom/videogrep/archive/master.zip',
       author = 'Sam Lavigne',
       author_email = 'splavigne@gmail.com',
       test_requires = ['pytest==2.5.2'],
