7249d05c9b7c54def9cae8478bcf6db82d5f9f58
Dean Silfen <dean.silfen@gmail.com>
2014-07-27 23:39:58 -0400

    changed video_downloader helper to better fit my own needs

--- videogrep/tools/video_downloader.py
+++ videogrep/tools/video_downloader.py
@@ -10,24 +10,36 @@ def get_urls(url):
     data = urllib.urlopen(url).read()
     soup = BeautifulSoup(data)
     soupy_links = soup.select('a.yt-uix-sessionlink')
-    urls = ['http://youtube.com' + link.get('href') for link in soupy_links if 'watch' in link.get('href')]
+    urls = ['http://youtube.com' + link.get('href') for link in soupy_links\
+                                                if 'watch' in link.get('href')]
     return set(urls)
 
+def get_single_url(url, youtube_object=YouTube()):
+#    try:
+    youtube_object.url = url
+    video = youtube_object.get('mp4', '720p')
+    filename = youtube_object.filename
+    video.download()
+    cc = getyoutubecc.getyoutubecc(youtube_object.video_id,'en')
+    cc.writeSrtFile(filename + '.srt')
+#    except:
+#        return 'Broke!' 
 
-search_url = sys.argv[1]
-urls = get_urls(search_url)
-yt = YouTube()
-for url in urls:
-    try:
-        print "downloading " + url
-        yt.url = url
-        video = yt.get('mp4', '720p')
-        filename = yt.filename
-        video.download()
-        time.sleep(.1)
-        cc = getyoutubecc.getyoutubecc(yt.video_id,'en')
-        cc.writeSrtFile(filename + '.srt')
-        time.sleep(.1)
-    except:
-        print "couldn't download " + url
 
+def search_all_files(search_url, youtube_object=YouTube()):
+    search_url = search_url 
+    urls = get_urls(search_url)
+    yt = youtube_object
+    for url in urls:
+        try:
+            print "downloading " + url
+            yt.url = url
+            video = yt.get('mp4', '720p')
+            filename = yt.filename
+            video.download()
+            time.sleep(.1)
+            cc = getyoutubecc.getyoutubecc(yt.video_id,'en')
+            cc.writeSrtFile(filename + '.srt')
+            time.sleep(.1)
+        except:
+            print "couldn't download " + url
