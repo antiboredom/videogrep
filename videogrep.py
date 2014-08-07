5395a2ac568355a60fb090625bf6ba5a8964ab7b
d8sconz <d8sconz@gmail.com>
2014-08-07 15:52:45 +0700

    Multiple search terms and whole word search
    Now takes multiple search expressions and words, delimited by commas. For example search: videogrep.py --input "E:\Movies\Pulp Fiction\Pulp_Fiction.mp4" --search "hello,hi,how are you,howdy". Also short expressions such as 'Hi' can be searched for without returning words like 'tHIs' or 'sHIt' etc.

--- videogrep.py
+++ videogrep.py
@@ -1,4 +1,4 @@
-FFMPEG_BINARY = '/usr/local/bin/ffmpeg'
+FFMPEG_BINARY = r'D:\ffmpeg\bin\ffmpeg'
 
 import os
 import re
@@ -21,7 +21,6 @@ def convert_timespan(timespan):
     end = convert_timestamp(end)
     return start, end
 
-
 def convert_timestamp(timestamp):
     """Converts an srt timestamp into seconds"""
     timestamp = timestamp.strip()
@@ -33,7 +32,6 @@ def convert_timestamp(timestamp):
     seconds = seconds + hours * 60 * 60 + minutes * 60 + float(millis) / 1000
     return seconds
 
-
 def clean_srt(srt):
     """Removes damaging line breaks and numbers from srt files and returns a dictionary"""
     with open(srt, 'r') as f:
@@ -42,7 +40,6 @@ def clean_srt(srt):
     lines = text.splitlines()
     output = OrderedDict()
     key = ''
-
     for line in lines:
         line = line.strip()
         if line.find('-->') > -1:
@@ -51,16 +48,13 @@ def clean_srt(srt):
         else:
             if key != '':
                 output[key] += line + ' '
-
     return output
-
 
 def cleanup_log_files(outputfile):
     d = os.path.dirname(os.path.abspath(outputfile))
     logfiles = [f for f in os.listdir(d) if f.endswith('ogg.log')]
     for f in logfiles:
         os.remove(f)
-
 
 def demo_supercut(composition, padding):
     for i, c in enumerate(composition):
@@ -71,26 +65,21 @@ def demo_supercut(composition, padding):
             start = start + padding
         print "{1} to {2}:\t{0}".format(line, start, end)
 
-
 def create_supercut(composition, outputfile, padding):
     print ("[+] Creating clips.")
     demo_supercut(composition, padding)
-
     # add padding when necessary
     for (clip, nextclip) in zip(composition, composition[1:]):
         if ((nextclip['file'] == clip['file']) and (nextclip['start'] < clip['end'])):
             nextclip['start'] += padding
-
-    # put all clips together:
+    # put all clips together
     all_filenames = set([c['file'] for c in composition])
     videofileclips = dict([(f, VideoFileClip(f)) for f in all_filenames])
     cut_clips = [videofileclips[c['file']].subclip(c['start'], c['end']) for c in composition]
-
     print "[+] Concatenating clips."
     final_clip = concatenate(cut_clips)
-
     print "[+] Writing ouput file."
-    final_clip.to_videofile(outputfile, codec="libx264")
+    final_clip.to_videofile(outputfile, codec="libx264", audio_codec="libmp3lame")
 
 
 def create_supercut_in_batches(composition, outputfile, padding):
@@ -110,65 +99,56 @@ def create_supercut_in_batches(composition, outputfile, padding):
             start_index += batch_size
             end_index += batch_size
             next
-
     clips = [VideoFileClip(filename) for filename in batch_comp]
     video = concatenate(clips)
-    video.to_videofile(outputfile)
-
+    video.to_videofile(outputfile,audio_codec="libmp3lame")
     #remove partial video files
     for filename in batch_comp:
         os.remove(filename)
-
     cleanup_log_files(outputfile)
 
+def findWholeWord(srch):
+    return re.compile(r'\b({0})\b'.format(srch), flags=re.IGNORECASE).search
 
 def search_line(line, search, searchtype):
     if searchtype == 're':
-        return re.search(search, line)  #, re.IGNORECASE)
+        return findWholeWord(search)(line)  #, re.IGNORECASE)
     elif searchtype == 'pos':
         return Search.search_out(line, search)
     elif searchtype == 'hyper':
         return Search.hypernym_search(line, search)
 
-
 def get_subtitle_files(inputfile):
     """Returns a list of subtitle files"""
     srts = []
-
     if os.path.isfile(inputfile):
         filename = inputfile.split('.')
         filename[-1] = 'srt'
         srts = ['.'.join(filename)]
-
     elif os.path.isdir(inputfile):
         if inputfile.endswith('/') == False:
             inputfile += '/'
         srts = [inputfile + f for f in os.listdir(inputfile) if f.lower().endswith('srt')]
-
     else:
         print "[!] No subtitle files were found."
         exit(1)
-
     return srts
 
 
 def videogrep(inputfile, outputfile, search, searchtype, maxclips=0, padding=0, test=False, randomize=False, sync=0):
     srts = get_subtitle_files(inputfile)
-
     padding = padding / 1000.0
     sync = sync / 1000.0
     composition = []
     foundSearchTerm = False
-
+    searchTerms = search.split(',')
+	
     # Iterate over each subtitles file.
     for srt in srts:
-
         print srt
         lines = clean_srt(srt)
-
         videofile = ""
         foundVideoFile = False
-
         print "[+] Searching for video file corresponding to '" + srt + "'."
         for ext in usable_extensions:
             tempVideoFile = srt.replace('.srt', '.' + ext)
@@ -176,38 +156,29 @@ def videogrep(inputfile, outputfile, search, searchtype, maxclips=0, padding=0, 
                 videofile = tempVideoFile
                 foundVideoFile = True
                 print "[+] Found '" + tempVideoFile + "'."
-
         # If a correspndong video file was found for this subtitles file...
         if foundVideoFile:
-
             # Check that the subtitles file contains subtitles.
             if lines:
-
                 # Iterate over each line in the current subtitles file.
                 for timespan in lines.keys():
                     line = lines[timespan].strip()
-
                     # If this line contains the search term
-                    if search_line(line, search, searchtype):
-
-                        foundSearchTerm = True
-
-                        # Extract the timespan for this subtitle.
-                        start, end = convert_timespan(timespan)
-                        start = start + sync - padding
-                        end = end + sync + padding
-
-                        # Record this occurance of the search term.
-                        composition.append({'file': videofile, 'time': timespan, 'start': start, 'end': end, 'line': line})
-
+                    for term in searchTerms:					
+                        if search_line(line, term, searchtype):
+                            foundSearchTerm = True
+                            # Extract the timespan for this subtitle.
+                            start, end = convert_timespan(timespan)
+                            start = start + sync - padding
+                            end = end + sync + padding
+                            # Record this occurance of the search term.
+                            composition.append({'file': videofile, 'time': timespan, 'start': start, 'end': end, 'line': line})
                 # If the search was unsuccessful.
                 if foundSearchTerm == False:
                     print "[!] Search term '" + search + "'" + " was not found is subtitle file '" + srt + "'."
-
             # If no subtitles were found in the current file.
             else:
                 print "[!] Subtitle file '" + srt + "' is empty."
-
         # If no video file was found...
         else:
             print "[!] No video file was found which corresponds to subtitle file '" + srt + "'."
@@ -216,21 +187,16 @@ def videogrep(inputfile, outputfile, search, searchtype, maxclips=0, padding=0, 
             for ext in usable_extensions:
                 extList += ext + ", "
             print extList
-
     # If the search term was not found in any subtitle file...
     if foundSearchTerm == False:
         print "[!] Search term '" + search + "'" + " was not found in any file."
         exit(1)
-
     else:
         print "[+] Search term '" + search + "'" + " was found in " + str(len(composition)) + " places."
-
         if maxclips > 0:
             composition = composition[:maxclips]
-
         if randomize == True:
             random.shuffle(composition)
-
         if test == True:
             demo_supercut(composition, padding)
         else:
@@ -240,10 +206,8 @@ def videogrep(inputfile, outputfile, search, searchtype, maxclips=0, padding=0, 
             else:
                 create_supercut(composition, outputfile, padding)
 
-
 if __name__ == '__main__':
     import argparse
-
     parser = argparse.ArgumentParser(description='Generate a "supercut" of one or more video files by searching through subtitle tracks.')
     parser.add_argument('--input', '-i', dest='inputfile', required=True, help='video or subtitle file, or folder')
     parser.add_argument('--search', '-s', dest='search', required=True, help='search term')
@@ -255,8 +219,5 @@ if __name__ == '__main__':
     parser.add_argument('--youtube', '-yt', help='grab clips from youtube based on your search')
     parser.add_argument('--padding', '-p', dest='padding', default=0, type=int, help='padding in milliseconds to add to the start and end of each clip')
     parser.add_argument('--resyncsubs', '-rs', dest='sync', default=0, type=int, help='Subtitle re-synch delay +/- in milliseconds')
-
     args = parser.parse_args()
-
     videogrep(args.inputfile, args.outputfile, args.search, args.searchtype, args.maxclips, args.padding, args.test, args.randomize, args.sync)
-
