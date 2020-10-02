3ca8227980961bc5be0ccfbb5fde9b38c09bedd8
AdnanSE <72250612+AdnanSE@users.noreply.github.com>
2020-10-02 13:38:21 +0500

    maths.py

---
+++ maths.py
@@ -0,0 +1,15 @@
+print(3 + 4)
+print(3.5 + 6.3)
+print(10 - 5)
+print(5 * 3)
+
+print("Qai" + "ser")
+print("hi" * 32)
+
+print(10 + 3.8)
+
+print(5 ** 3)
+print(4 ** 4)
+
+print(3 + 5 * 3)
+print((3 + 5) * 3)
