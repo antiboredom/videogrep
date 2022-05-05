b363cfa310c0237d58bc7100efada7a068ee5460
Sam Lavigne <splavigne@gmail.com>
2022-05-04 20:32:42 -0400

    better xml export

--- videogrep/fcpxml.py
+++ videogrep/fcpxml.py
@@ -1,212 +1,228 @@
 from moviepy.editor import VideoFileClip
+from functools import lru_cache
 import os
 
 
-template = """<?xml version="1.0" encoding="UTF-8"?>
-<xmeml version="4">
-  <project>
-    <name>{project_name}</name>
-    <children>
-      <sequence id="sequence-1">
-        <name>{project_name}</name>
-        <duration>{track_duration}</duration>
-        <rate>
-          <timebase>{fps}</timebase>
-          <ntsc>FALSE</ntsc>
-        </rate>
-        <media>
-          <video>
-            <track>
-                {clips}
-            </track>
-            <format>
-              <samplecharacteristics>
-                <width>{width}</width>
-                <height>{height}</height>
-                <pixelaspectratio>square</pixelaspectratio>
-              </samplecharacteristics>
-            </format>
-          </video>
-          <audio>
-            <track>
-                {audio_clips}
-            </track>
-          </audio>
-        </media>
-      </sequence>
-    </children>
-  </project>
-</xmeml>
-"""
-
-file_template = """
-<file id="{file_id}">
-  <pathurl>file://{full_path}</pathurl>
-  <name>{base_path}</name>
-  <rate>
-    <timebase>{fps}</timebase>
-    <ntsc>FALSE</ntsc>
-  </rate>
-  <duration>{total_duration}</duration>
-  <timecode>
-    <rate>
-      <timebase>{fps}</timebase>
-      <ntsc>FALSE</ntsc>
-    </rate>
-    <string>00:00:00:00</string>
-    <frame>0</frame>
-    <displayformat>NDF</displayformat>
-  </timecode>
-  <media>
-    <video />
-    <audio />
-  </media>
-</file>
-"""
-
-clip_item_template = """
-<clipitem frameBlend="FALSE" id="{clip_item_id}">
-{file_meta}
-<name>{shot_name}</name>
-<rate>
-  <timebase>{fps}</timebase>
-  <ntsc>FALSE</ntsc>
-</rate>
-<duration>{clip_duration}</duration>
-<start>{start}</start>
-<end>{end}</end>
-<in>{clip_in}</in>
-<out>{clip_out}</out>
-</clipitem>
-"""
-
-audio_item_template = """
-<clipitem id="{clip_item_id}">
-{file_meta}
-<name>{shot_name}</name>
-<rate>
-  <timebase>{fps}</timebase>
-  <ntsc>FALSE</ntsc>
-</rate>
-<duration>{clip_duration}</duration>
-<start>{start}</start>
-<end>{end}</end>
-<in>{clip_in}</in>
-<out>{clip_out}</out>
-</clipitem>
-
-"""
+@lru_cache(maxsize=None)
+def get_clip(filename):
+    return VideoFileClip(filename)
 
 
-
-def get_clip_meta(segments):
-    v_id = 1
-
-    meta = {}
-
-    for s in segments:
-        filename = s["file"]
-
-        if filename not in meta:
-            _v = VideoFileClip(filename)
-            meta[filename] = {
-                "duration": _v.duration,
-                "fps": _v.fps,
-                "v_id": f"file-{v_id}",
-                "width": _v.w,
-                "height": _v.h,
-            }
-            v_id += 1
-
-    return meta
-
-
-def secs_to_frames(seconds, fps):
+def frames(seconds, fps):
     return int(seconds * fps)
 
 
-def compose(segments, outname):
-    meta = get_clip_meta(segments)
+class Clip:
+    def __init__(self, filename, clip_id, start, end, clip_in, clip_out):
+        self.clip = get_clip(filename)
+        self.full_path = os.path.abspath(filename)
+        self.base_path = os.path.basename(filename)
+        self.shot_name = os.path.basename(filename)
+        self.clip_id = f"{self.shot_name}-{clip_id}"
+        self.audio_clip_id = f"{self.clip_id}-audio"
+        self.fps = self.clip.fps
+        self.duration = self.clip.duration
+        self.width = self.clip.w
+        self.height = self.clip.h
+        self.start = self.frames(start)
+        self.end = self.frames(end)
+        self.clip_in = self.frames(clip_in)
+        self.clip_out = self.frames(clip_out)
+        self.file_id = self.shot_name
+        self.audio_channels = 2
 
-    start = 0
-    track_duration = 0
+    def frames(self, secs):
+        return int(secs * self.fps)
 
-    clip_items = []
-    audio_clip_items = []
-    files = {}
+    def render(self, include_file=False):
+        file_meta = f'<file id="{self.file_id}" />'
 
-    clip_id = 1
+        if include_file:
+            file_meta = self.render_file()
 
-    total_clips = len(segments)
+        return f"""
+          <clipitem id="{self.clip_id}">
+            <name>{self.shot_name}</name>
+            <duration>{self.duration}</duration>
+            <rate>
+              <timebase>{self.fps}</timebase>
+              <ntsc>FALSE</ntsc>
+            </rate>
+            <start>{self.start}</start>
+            <end>{self.end}</end>
+            <enabled>TRUE</enabled>
+            <in>{self.clip_in}</in>
+            <out>{self.clip_out}</out>
+            {file_meta}
+            <compositemode>normal</compositemode>
+            <link>
+              <linkclipref>{self.clip_id}</linkclipref>
+            </link>
+            <link>
+              <linkclipref>{self.audio_clip_id}</linkclipref>
+            </link>
+            <comments />
+          </clipitem>"""
 
-    for s in segments:
-        filename = s["file"]
-        clip_in = s["start"]
-        clip_out = s["end"]
-        clip_duration = s["end"] - s["start"]
-        end = start + clip_duration
-        track_duration += clip_duration
+    def render_audio(self):
+        return f"""
+          <clipitem id="{self.audio_clip_id}">
+            <name>{self.shot_name}</name>
+            <duration>{self.duration}</duration>
+            <rate>
+              <timebase>{self.fps}</timebase>
+              <ntsc>FALSE</ntsc>
+            </rate>
+            <start>{self.start}</start>
+            <end>{self.end}</end>
+            <enabled>TRUE</enabled>
+            <in>{self.clip_in}</in>
+            <out>{self.clip_out}</out>
+            <file id="{self.file_id}" />
+            <sourcetrack>
+              <mediatype>audio</mediatype>
+              <trackindex>1</trackindex>
+            </sourcetrack>
+            <link>
+              <linkclipref>{self.clip_id}</linkclipref>
+            </link>
+            <link>
+              <linkclipref>{self.audio_clip_id}</linkclipref>
+            </link>
+            <comments />
+          </clipitem>"""
 
-        total_clip_duration = meta[filename]["duration"]
-        width = meta[filename]['width']
-        height = meta[filename]['height']
-        fps = meta[filename]["fps"]
-        file_id = meta[filename]["v_id"]
+    def render_file(self):
+        return f"""
+            <file id="{self.file_id}">
+              <pathurl>file://{self.full_path}</pathurl>
+              <name>{self.base_path}</name>
+              <rate>
+                <timebase>{self.fps}</timebase>
+                <ntsc>FALSE</ntsc>
+              </rate>
+              <duration>{self.duration}</duration>
+              <timecode>
+                <rate>
+                  <timebase>{self.fps}</timebase>
+                  <ntsc>FALSE</ntsc>
+                </rate>
+                <string>00:00:00:00</string>
+                <displayformat>NDF</displayformat>
+              </timecode>
+              <media>
+                <video>
+                  <duration>{self.duration}</duration>
+                  <samplecharacteristics>
+                    <width>{self.width}</width>
+                    <height>{self.height}</height>
+                  </samplecharacteristics>
+                </video>
+                <audio>
+                  <channelcount>{self.audio_channels}</channelcount>
+                </audio>
+              </media>
+            </file>"""
 
-        if file_id not in files:
-            files[file_id] = file_template.format(
-                file_id=file_id,
-                full_path=filename,
-                base_path=os.path.basename(filename),
-                fps=fps,
-                total_duration=secs_to_frames(total_clip_duration, fps),
+
+class Sequence:
+    def __init__(self, segments, project_name):
+        clips = []
+
+        start = 0
+        track_duration = 0
+        for index, s in enumerate(segments):
+            clip_duration = s["end"] - s["start"]
+            end = start + clip_duration
+            track_duration += clip_duration
+
+            clip = Clip(
+                filename=s["file"],
+                clip_id=index,
+                clip_in=s["start"],
+                clip_out=s["end"],
+                start=start,
+                end=end,
             )
-            file_clip = files[file_id]
-        else:
-            file_clip = f'<file id="{file_id}" />'.format(file_id)
+            clips.append(clip)
+            start = end
 
-        clip = clip_item_template.format(
-            clip_item_id=f"clipitem-{clip_id}",
-            file_meta=file_clip,
-            shot_name=os.path.basename(filename),
-            fps=fps,
-            clip_duration=secs_to_frames(clip_duration, fps),
-            start=secs_to_frames(start, fps),
-            end=secs_to_frames(end, fps),
-            clip_in=secs_to_frames(clip_in, fps),
-            clip_out=secs_to_frames(clip_out, fps),
-        )
+        self.clips = clips
+        self.track_duration = clips[0].frames(track_duration)
+        self.project_name = project_name
+        self.fps = clips[0].fps
+        self.width = clips[0].width
+        self.height = clips[0].height
+
+    def render_video(self):
+        files = []
+        rendered = []
+        for c in self.clips:
+            if c.full_path in files:
+                need_file = False
+            else:
+                files.append(c.full_path)
+                need_file = True
+
+            rendered.append(c.render(include_file=need_file))
+
+        return "\n".join(rendered)
+
+    def render_audio(self):
+        return "\n".join([c.render_audio() for c in self.clips])
+
+    def render(self):
+        return f"""<?xml version="1.0" encoding="utf-8"?>
+            <!DOCTYPE xmeml>
+            <xmeml version="5">
+              <sequence>
+                <name>{self.project_name}</name>
+                <duration>{self.track_duration}</duration>
+                <rate>
+                  <timebase>{self.fps}</timebase>
+                  <ntsc>FALSE</ntsc>
+                </rate>
+                <in>-1</in>
+                <out>-1</out>
+                <timecode>
+                  <string>01:00:00:00</string>
+                  <frame>108000</frame>
+                  <displayformat>NDF</displayformat>
+                  <rate>
+                    <timebase>{self.fps}</timebase>
+                    <ntsc>TRUE</ntsc>
+                  </rate>
+                </timecode>
+                <media>
+                  <video>
+                    <track>
+                        {self.render_video()}
+                    </track>
+                    <format>
+                      <samplecharacteristics>
+                        <width>{self.width}</width>
+                        <height>{self.height}</height>
+                        <pixelaspectratio>square</pixelaspectratio>
+                        <rate>
+                          <timebase>{self.fps}</timebase>
+                          <ntsc>FALSE</ntsc>
+                        </rate>
+                      </samplecharacteristics>
+                    </format>
+                  </video>
+                  <audio>
+                    <track>
+                        {self.render_audio()}
+                    </track>
+                  </audio>
+                </media>
+              </sequence>
+            </xmeml>"""
 
 
-        audio_clip = audio_item_template.format(
-            clip_item_id=f"clipitem-{clip_id+total_clips}",
-            file_meta=files[file_id],
-            shot_name=os.path.basename(filename),
-            fps=fps,
-            clip_duration=secs_to_frames(clip_duration, fps),
-            start=secs_to_frames(start, fps),
-            end=secs_to_frames(end, fps),
-            clip_in=secs_to_frames(clip_in, fps),
-            clip_out=secs_to_frames(clip_out, fps),
-        )
-
-        clip_items.append(clip)
-        audio_clip_items.append(audio_clip)
-
-        clip_id += 1
-
-        start = end
-
-    output = template.format(
-        fps=fps,
-        width=width,
-        height=height,
-        project_name=os.path.splitext(os.path.basename(outname))[0],
-        clips="\n".join(clip_items),
-        audio_clips="\n".join(audio_clip_items),
-        track_duration=secs_to_frames(track_duration, fps),
-    )
+def compose(segments, outname):
+    s = Sequence(segments, outname)
 
     # output = minidom.parseString(output)
     with open(outname, "w") as outfile:
-        outfile.write(output)
+        outfile.write(s.render())
