bf5e6ace6457406f52148e2387b98546999f596a
Sam Lavigne <splavigne@gmail.com>
2022-05-24 14:33:24 -0400

    bump version

--- tests/test_videogrep.py
+++ tests/test_videogrep.py
@@ -13,7 +13,7 @@ def File(path):
 
 
 def test_version():
-    assert videogrep.__version__ == "2.0.1"
+    assert videogrep.__version__ == "2.0.2"
 
 
 def test_srts():
--- videogrep/__init__.py
+++ videogrep/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "2.0.1"
+__version__ = "2.0.2"
 
 from . import vtt, srt, sphinx, fcpxml
 from .videogrep import (
