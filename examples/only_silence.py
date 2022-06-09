180b4f1871ec7a7bf068e7db5e679b740b8a67ab
Sam Lavigne <splavigne@gmail.com>
2022-06-09 12:41:10 -0400

    fix only silences example

--- examples/only_silence.py
+++ examples/only_silence.py
@@ -11,28 +11,29 @@ adjuster = 0.1
 
 filenames = sys.argv[1:]
 
-timestamps = parse_transcript(filenames)
-
 silences = []
+for filename in filenames:
+    timestamps = parse_transcript(filename)
 
-# this uses the sentences in the transcript
-# for sentence1, sentence2 in zip(timestamps[:-2], timestamps[1:]):
-#     start = sentence1['end']
-#     end = sentence2['start'] - adjuster
-#     duration = end - start
-#     if duration > min_duration and duration < max_duration:
-#        silences.append({'start': start, 'end': end, 'file': filename})
 
-# this uses the words, if available
-words = []
-for sentence in timestamps:
-    words += sentence['words']
+    # this uses the sentences in the transcript
+    # for sentence1, sentence2 in zip(timestamps[:-2], timestamps[1:]):
+    #     start = sentence1['end']
+    #     end = sentence2['start'] - adjuster
+    #     duration = end - start
+    #     if duration > min_duration and duration < max_duration:
+    #        silences.append({'start': start, 'end': end, 'file': filename})
 
-for word1, word2 in zip(words[:-2], words[1:]):
-    start = word1['end']
-    end = word2['start'] - adjuster
-    duration = end - start
-    if duration > min_duration and duration < max_duration:
-        silences.append({'start': start, 'end': end, 'file': filename})
+    # this uses the words, if available
+    words = []
+    for sentence in timestamps:
+        words += sentence['words']
+
+    for word1, word2 in zip(words[:-2], words[1:]):
+        start = word1['end']
+        end = word2['start'] - adjuster
+        duration = end - start
+        if duration > min_duration and duration < max_duration:
+            silences.append({'start': start, 'end': end, 'file': filename})
 
 create_supercut(silences, 'silences.mp4')
